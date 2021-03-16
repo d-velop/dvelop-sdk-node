b006e573d2997038fc555f511a5804dc7ac7739c
Lennart <lennart.klose@d-velop.de>
2021-03-16 11:51:53 +0100

    Switched jsdoc to typedoc

--- packages/app-router/src/generate-request-id/generate-request-id.ts
+++ packages/app-router/src/generate-request-id/generate-request-id.ts
@@ -2,12 +2,12 @@ import { v4 } from "uuid";
 
 /**
  * Returns a unique Version 4 UUID which can be used as requestId.
- * @category @dvelop-sdk/app-router
  * @returns {string}
  *
- * @example
+ * @example ```typescript
  * const requestId: string = generateRequestId();
  * console.log(requestId); //ac25ae73-f4b6-477e-a5fa-877c8dea863d
+ * ```
  */
 export function generateRequestId(): string {
   return v4();
--- packages/app-router/src/index.ts
+++ packages/app-router/src/index.ts
@@ -1,6 +1,27 @@
+/**
+ * This module contains functionality for the App-Router in the d.velop cloud.
+ * @module app-router
+ */
+
+/**
+ * HTTP Header-Name for the systemBaseUri for the tenant.
+ */
 export const DVELOP_SYSTEM_BASE_URI_HEADER = "x-dv-baseuri";
+
+/**
+ * HTTP Header-Name for the tenantId for the tenant.
+ */
 export const DVELOP_TENANT_ID_HEADER = "x-dv-tenant-id";
+
+/**
+ * HTTP Header-Name for the requestId for the request. Forward this id to make calls trackable throughout d.velop apps.
+ */
 export const DVELOP_REQUEST_ID_HEADER = "x-dv-sig-1";
+
+/**
+ * HTTP Header-Name for the requestSignature for the request. **The requestSignature should be validated for every request when recieving calls from the d.velop cloud.
+ * Refer to the [d.velop basics tenant header section](https://developer.d-velop.de/dev/en/basics#tenant-header) for more information.**
+ */
 export const DVELOP_REQUEST_SIGNATURE_HEADER = "x-dv-request-id";
 
 export { generateRequestId } from "./generate-request-id/generate-request-id";
