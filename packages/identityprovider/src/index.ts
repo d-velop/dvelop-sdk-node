c3ed7201fc083ae03fdbd5ec9d7f64f02b1995c5
Lennart <lennart.klose@d-velop.de>
2021-03-30 16:46:51 +0200

    Export members

--- packages/identityprovider/src/index.ts
+++ packages/identityprovider/src/index.ts
@@ -2,5 +2,7 @@
  * This module contains functionality for the [Identityprovider-App](https://developer.d-velop.de/documentation/idpapi/en/identityprovider-app-201523580.html) in the d.velop cloud.
  * @module identityprovider
  */
+export { AuthSession } from "./get-authsession/auth-session";
+export { getAuthSession } from "./get-authsession/get-auth-session";
 export { ScimUser } from "./validate-authsession-id/scrim-user";
 export { validateAuthsessionId } from "./validate-authsession-id/validate-authsession-id";
