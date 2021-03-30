5d3b4ab6d842fba6c10f110e85ab19f4c365f1f0
Lennart <lennart.klose@d-velop.de>
2021-03-30 16:31:55 +0200

    getAuthsession Documentation

--- packages/identityprovider/src/get-authsession/authsession.ts
+++ packages/identityprovider/src/get-authsession/authsession.ts
@@ -1,3 +1,8 @@
+/**
+ * A valid authentication-session for the d.velop cloud. Refer to the [documentation](https://developer.d-velop.de/documentation/idpapi/en/identityprovider-app-201523580.html#validating-the-login) for further information.
+ * @property {string} id Id for the Authsession. This Key will validate your requests. **The AuthsessionId should be kept secret and never be publicly available**
+ * @property {Date} expire Date-Object for the point in time at which the AuthsessionId will no longer be valid.
+ */
 export interface Authsession {
   id: string;
   expire: Date;
--- packages/identityprovider/src/get-authsession/get-authsession.ts
+++ packages/identityprovider/src/get-authsession/get-authsession.ts
@@ -7,12 +7,16 @@ export interface AuthsessionDto {
 }
 
 /**
- * Returns an authSessionId based on an API-Key.
+ * Returns an [AuthSession]{@link AuthSession} based on an API-Key.
+ * **The AuthSessionId should be kept secret and never be publicly available**
  * @param {string} systemBaseUri SystemBaseUri for the tenant.
  * @param {string} apiKey API-Key provided by the d.velop cloud.
  * @returns {string}
  *
  * @example ```typescript
+ * const authsession: Authsession = getAuthsession("https://monster-ag.d-velop.cloud", "<API_KEY>");
+ * console.log(authsession.id); //return a valid authsessionId
+ * console.log('still valid:', authsession.expire.getTime() > new Date().getTime()); //still valid: true
  * ```
  */
 export async function getAuthsession(systemBaseUri: string, apiKey: string): Promise<Authsession> {
