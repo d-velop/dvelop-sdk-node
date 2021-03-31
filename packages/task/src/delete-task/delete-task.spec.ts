8a57343674925b42689d3a61d3735e07ea00744d
Patrick Rose <rose.patrick@gmx.de>
2021-03-31 18:16:39 +0200

    Delete task with location or task object

--- packages/task/src/delete-task/delete-task.spec.ts
+++ packages/task/src/delete-task/delete-task.spec.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { Task } from "../task";
 import { deleteTask } from "./delete-task";
 
 jest.mock("axios");
@@ -11,24 +12,39 @@ describe("deleteTask", () => {
     mockedAxios.delete.mockReset();
   });
   
-  it("should make Delete with correct URI", async () => {
+  it("should make Delete with correct URI with location given", async () => {
   
     const systemBaseUri = "HiItsMeSystemBaseUri";
     const authessionId = "HiItsMeAuthsessionId";
-    const id = "1234567890";
+    const location = "/it/is/a/location/1234567890";
   
-    await deleteTask(systemBaseUri, authessionId, id);
+    await deleteTask(systemBaseUri, authessionId, location);
   
-    expect(mockedAxios.delete).toBeCalledWith(`${systemBaseUri}/task/tasks/${id}`, expect.any(Object));
+    expect(mockedAxios.delete).toBeCalledWith(`${systemBaseUri}${location}`, expect.any(Object));
+  });
+
+  it("should make Delete with correct URI with task object given", async () => {
+  
+    const systemBaseUri = "HiItsMeSystemBaseUri";
+    const authessionId = "HiItsMeAuthsessionId";
+    const myTask:Task = {
+      location: "/it/is/a/location/1234567890",
+      subject: "Nice Subject",
+      description: "a description",
+    };
+  
+    await deleteTask(systemBaseUri, authessionId, myTask);
+  
+    expect(mockedAxios.delete).toBeCalledWith(`${systemBaseUri}${myTask.location}`, expect.any(Object));
   });
 
   it("should make Delete with correct headers", async () => {
 
     const authessionId = "HiItsMeAuthsessionId";
     const systemBaseUri = "HiItsMeSystemBaseUri";
-    const id = "1234567890";
+    const location = "/it/is/a/location/1234567890";
   
-    await deleteTask(systemBaseUri, authessionId, id);
+    await deleteTask(systemBaseUri, authessionId, location);
   
     expect(mockedAxios.delete).toBeCalledWith(expect.any(String), { headers: { "Authorization": `Bearer ${authessionId}` , "Origin" : systemBaseUri} });
   });
--- packages/task/src/delete-task/delete-task.ts
+++ packages/task/src/delete-task/delete-task.ts
@@ -1,23 +1,32 @@
 import axios from "axios";
+import { Task, instanceOfTask } from "../task";
 
 /**
  * Delete Task for given Task id
  * @param {string} systemBaseUri SystemBaseUri for the tenant.
  * @param {string} authsessionId AuthsessionId the call is executed with.
- * @param {string} id Id of the task to be deleted.
- *
+ * @param {string|Task} task Location of a task or task or task object.
+ * 
  * @example ```typescript
- * const taskId = "1234567890"
- * deleteTask("https://monster-ag.d-velop.cloud", "vXo4FMlnYYiGArNfjfJHEJDNWfjfejglgnewjgrjokgajifahfhdahfuewfhlR/4FxJxmNsjlq2XgWQm/GYVBq/hEvsJy1BK4WLoCXek=&ga8gds7gafkajgkj24ut8ugudash34jGlDG&dr6j0zusHVN8PcyerI0YXqRu30f8AGoUaZ6vInCDtZInS6aK2PplAelsv9t8", taskId);
+ * const taskLocation = "/it/is/a/location/1234567890"
+ * deleteTask("https://monster-ag.d-velop.cloud", "vXo4FMlnYYiGArNfjfJHEJDNWfjfejglgnewjgrjokgajifahfhdahfuewfhlR/4FxJxmNsjlq2XgWQm/GYVBq/hEvsJy1BK4WLoCXek=&ga8gds7gafkajgkj24ut8ugudash34jGlDG&dr6j0zusHVN8PcyerI0YXqRu30f8AGoUaZ6vInCDtZInS6aK2PplAelsv9t8", taskLocation);
  * ```
  */
 
 export async function deleteTask(
   systemBaseUri: string,
   authsessionId: string, 
-  id: string,
+  task: string | Task
 ): Promise<void> {
-  await axios.delete(`${systemBaseUri}/task/tasks/${id}`, {
+
+  var location:string;
+  if(instanceOfTask(task)){
+    location = task.location;
+  } else {
+    location = task;
+  }
+
+  await axios.delete(`${systemBaseUri}${location}`, {
     headers: {
       "Authorization": `Bearer ${authsessionId}`,
       "Origin" : systemBaseUri
