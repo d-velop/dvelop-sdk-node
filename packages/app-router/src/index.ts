736f5f193bd89b73a19c2d2886e092cf0be6c45a
Lennart <lennart.klose@d-velop.de>
2021-04-13 13:00:33 +0200

    Fixed a bug where headers were swapped.

--- packages/app-router/src/index.ts
+++ packages/app-router/src/index.ts
@@ -16,13 +16,13 @@ export const DVELOP_TENANT_ID_HEADER = "x-dv-tenant-id";
 /**
  * HTTP Header-Name for the requestId for the request. Forward this id to make calls trackable throughout d.velop apps.
  */
-export const DVELOP_REQUEST_ID_HEADER = "x-dv-sig-1";
+export const DVELOP_REQUEST_ID_HEADER = "x-dv-request-id";
 
 /**
  * HTTP Header-Name for the requestSignature for the request. **The requestSignature should be validated for every request when recieving calls from the d.velop cloud.
  * Refer to the [d.velop basics tenant header section](https://developer.d-velop.de/dev/en/basics#tenant-header) for more information.**
  */
-export const DVELOP_REQUEST_SIGNATURE_HEADER = "x-dv-request-id";
+export const DVELOP_REQUEST_SIGNATURE_HEADER = "x-dv-sig-1";
 
 export { generateRequestId } from "./generate-request-id/generate-request-id";
 export { validateRequestSignature } from "./validate-request-signature/validate-request-signature";
