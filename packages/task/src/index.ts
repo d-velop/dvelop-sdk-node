74e484314432b663f8d61f066e6ac9356cd4d8de
Lennart <lennart.klose@d-velop.de>
2021-04-27 16:42:53 +0200

    Readme changes

--- packages/task/src/index.ts
+++ packages/task/src/index.ts
@@ -1,5 +1,25 @@
 /**
- * This module contains functionality for the [Task-App](https://developer.d-velop.de/documentation/taskapp/de) in the d.velop cloud.
+ * <div align="center">
+
+    <h1>@dvelop-sdk/task</h1>
+
+    <img alt="npm (scoped)" src="https://img.shields.io/npm/v/@dvelop-sdk/task?style=for-the-badge">
+
+    <img alt="GitHub" src="https://img.shields.io/badge/GitHub-dvelop--sdk--node-%23ff0844?logo=github&style=for-the-badge">
+
+    <img alt="license" src="https://img.shields.io/github/license/d-velop/dvelop-sdk-node?style=for-the-badge">
+
+    </br>
+
+    <p>This package contains functionality for the <a href="https://developer.d-velop.de/documentation/taskapp/en">Task-App</a> in the d.velop cloud.</p>
+
+    <a href="https://d-velop.github.io/dvelop-sdk-node/modules/task.html"><strong>Explore the docs »</strong></a>
+    </br>
+    <a href="https://www.npmjs.com/package/@dvelop-sdk/task"><strong>Install via npm »</strong></a>
+    </br>
+    <a href="https://github.com/d-velop/dvelop-sdk-node"><strong>Check us out on GitHub »</strong></a>
+
+ * </div>
  * @module task
  */
 export { createTask } from "./create-task/create-task";
