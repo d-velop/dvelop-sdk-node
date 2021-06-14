d79b837b6d08a84b19b9af234db586efd7fb9f21
Lennart <lennart.klose@d-velop.de>
2021-06-14 15:12:33 +0200

    Fix license check

--- scripts/check-licenses.ts
+++ scripts/check-licenses.ts
@@ -8,14 +8,17 @@ import { execSync } from "child_process";
 
   packages.forEach(p => {
     try {
-      execSync(`license-checker --onlyAllow Apache-2.0;MIT;ISC;BSD-2-Clause;BSD-3-Clause --start ./packages/${p} --production`);
+      execSync(`license-checker --onlyAllow "Apache-2.0;MIT;ISC;BSD-2-Clause;BSD-3-Clause" --start ./packages/${p} --production`);
+      console.log(`License check successful in package '${p}'`);
     } catch (e) {
-      console.error(`License check unsuccessful in '${p}': ${e}`);
+      console.error(`License check unsuccessful in package '${p}': ${e}`);
       error = true;
     }
   });
 
   if(error) {
     throw "License check unsuccessful";
+  } else {
+	  console.log("License check successful");
   }
 })();
