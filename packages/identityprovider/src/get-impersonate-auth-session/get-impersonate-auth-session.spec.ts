71eed40534459c99a86fe9b6ec43936f70691d97
Lennart <lennart.klose@d-velop.de>
2021-07-26 14:58:01 +0200

    Implemente getImpersonateAuthSession

---
+++ packages/identityprovider/src/get-impersonate-auth-session/get-impersonate-auth-session.spec.ts
@@ -0,0 +1,116 @@
+import axios, { AxiosResponse } from "axios";
+import { getImpersonateAuthSession, UnauthorizedError } from "../index";
+
+jest.mock("axios");
+
+describe("getImpersonateAuthSession", () => {
+
+  const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+  beforeEach(() => {
+    mockedAxios.get.mockReset();
+  });
+
+  describe("axios-params", () => {
+
+    beforeEach(() => {
+      mockedAxios.get.mockResolvedValue({
+        data: { authSessionId: "HiItsMeAuthSessionId" }
+      });
+    });
+
+    it("should send GET", async () => {
+      await getImpersonateAuthSession("HiItsMeSystemBaseUri", "HiItsMeAppSession", "HiItsMeUserId");
+      expect(mockedAxios.get).toHaveBeenCalledTimes(1);
+    });
+
+    it("should send to /identityprovider", async () => {
+      await getImpersonateAuthSession("HiItsMeSystemBaseUri", "HiItsMeAppSession", "HiItsMeUserId");
+      expect(mockedAxios.get).toHaveBeenCalledWith("/identityprovider/impersonatesession", expect.any(Object));
+    });
+
+    it("should send with systemBaseUri as BaseURL", async () => {
+      const systemBaseUri: string = "HiItsMeSystemBaseUri";
+      await getImpersonateAuthSession(systemBaseUri, "HiItsMeAppSession", "HiItsMeUserId");
+      expect(mockedAxios.get).toHaveBeenCalledWith(expect.any(String), expect.objectContaining({
+        baseURL: systemBaseUri
+      }));
+    });
+
+    it("should send with authSessionId as Authorization-Header", async () => {
+      const appSession: string = "HiItsMeAppSession";
+      await getImpersonateAuthSession("HiItsMeSystemBaseUri", appSession, "HiItsMeUserId");
+      expect(mockedAxios.get).toHaveBeenCalledWith(expect.any(String), expect.objectContaining({
+        headers: expect.objectContaining({ "Authorization": `Bearer ${appSession}` })
+      }));
+    });
+
+    it("should send with userId as param", async () => {
+      const userId: string = "HiItsMeUserId";
+      await getImpersonateAuthSession("HiItsMeSystemBaseUri", "HiItsMeAppSession", userId);
+      expect(mockedAxios.get).toHaveBeenCalledWith(expect.any(String), expect.objectContaining({
+        params: expect.objectContaining({ userId: userId })
+      }));
+    });
+  });
+
+  describe("response", () => {
+
+    it("should return user", async () => {
+
+      const authSessionId: string = "HiItsMeAuthSessionId";
+
+      mockedAxios.get.mockResolvedValue({
+        data: {
+          authSessionId: authSessionId
+        }
+      });
+
+      const result: string = await getImpersonateAuthSession("HiItsMeSystemBaseUri", "HiItsMeAppSession", "HiItsMeUserId");
+      expect(result).toEqual(authSessionId);
+    });
+  });
+
+  describe("errors", () => {
+
+    it("should throw UnauthorizesError on status 401", async () => {
+
+      const response: AxiosResponse = {
+        status: 401,
+      } as AxiosResponse;
+
+      mockedAxios.get.mockRejectedValue({ response });
+
+      let error: UnauthorizedError;
+      try {
+        await getImpersonateAuthSession("HiItsMeSystemBaseUri", "HiItsMeAppSession", "HiItsMeUserId");
+      } catch (e) {
+        error = e;
+      }
+
+      expect(error instanceof UnauthorizedError).toBeTruthy();
+      expect(error.message).toContain("Failed to impersonate user:");
+      expect(error.response).toEqual(response);
+    });
+
+    it("should throw UnknownErrors", async () => {
+
+      const errorString: string = "HiItsMeError";
+      const error: Error = new Error(errorString);
+      mockedAxios.get.mockImplementation(() => {
+        throw error;
+      });
+
+      let resultError: Error;
+      try {
+        await getImpersonateAuthSession("HiItsMeSystemBaseUri", "HiItsMeAppSession", "HiItsMeUserId");
+      } catch (e) {
+        resultError = e;
+      }
+
+      expect(resultError).toBe(error);
+      expect(resultError.message).toContain(errorString);
+      expect(resultError.message).toContain("Failed to impersonate user:");
+    });
+  });
+});
---
+++ packages/identityprovider/src/get-impersonate-auth-session/get-impersonate-auth-session.ts
@@ -0,0 +1,48 @@
+import axios from "axios";
+import { UnauthorizedError } from "../index";
+
+interface InpersonationAuthSessionDto {
+  authSessionId: string;
+}
+
+/**
+ * Provides a valid authSessionId for a user without login.
+ *
+ * @param {string} systemBaseUri SystemBaseUri for the tenant
+ * @param {string} appSession A valid appSession
+ * @param {string} userId User that should be impersonated
+ * @returns Valid authSessionId for the given user
+ *
+ * //TODO: example
+ */
+export async function getImpersonateAuthSession(systemBaseUri: string, appSession: string, userId: string): Promise<string> {
+
+  const errorContext: string = "Failed to impersonate user";
+
+  try {
+    const response = await axios.get<InpersonationAuthSessionDto>("/identityprovider/impersonatesession", {
+      baseURL: systemBaseUri,
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/hal+json",
+        "Authorization": `Bearer ${appSession}`
+      },
+      params: {
+        userId: userId
+      }
+    });
+
+    return response.data.authSessionId;
+  } catch (e) {
+    if (e.response) {
+      switch (e.response.status) {
+      case 401:
+        throw new UnauthorizedError(errorContext, e.response);
+
+        //TODO: 404 if user does not exist?
+      }
+    }
+    e.message = `${errorContext}: ${e.message}`;
+    throw e;
+  }
+}
