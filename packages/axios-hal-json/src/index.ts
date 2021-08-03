bdfc1589154d51c3053ecfc0323604b8b66b3d94
Lennart <lennart.klose@d-velop.de>
2021-08-03 08:31:07 +0200

    Further docs tuning for axios-hal-json

--- packages/axios-hal-json/src/index.ts
+++ packages/axios-hal-json/src/index.ts
@@ -1,17 +1,31 @@
 /**
 <div align="center">
   <h1>@dvelop-sdk/axios-hal-json</h1>
-  <img alt="npm (scoped)" src="https://img.shields.io/npm/v/@dvelop-sdk/axios-hal-json?style=for-the-badge">
-  <img alt="npm bundle size (scoped)" src="https://img.shields.io/bundlephobia/min/@dvelop-sdk/axios-hal-json?style=for-the-badge">
-  <img alt="GitHub" src="https://img.shields.io/badge/GitHub-dvelop--sdk--node-%23ff0844?logo=github&style=for-the-badge">
-  <img alt="license" src="https://img.shields.io/github/license/d-velop/dvelop-sdk-node?style=for-the-badge">
+  <a href="https://www.npmjs.com/package/@dvelop-sdk/axios-hal-json">
+    <img alt="npm (scoped)" src="https://img.shields.io/npm/v/@dvelop-sdk/axios-hal-json?style=for-the-badge">
+  </a>
+  <a href="https://www.npmjs.com/package/@dvelop-sdk/axios-hal-json">
+    <img alt="npm bundle size (scoped)" src="https://img.shields.io/bundlephobia/min/@dvelop-sdk/axios-hal-json?style=for-the-badge">
+  </a>
+  <a href="https://github.com/d-velop/dvelop-sdk-node">
+    <img alt="GitHub" src="https://img.shields.io/badge/GitHub-dvelop--sdk--node-%23ff0844?logo=github&style=for-the-badge">
+  </a>
+  <a href="https://github.com/d-velop/dvelop-sdk-node/blob/master/LICENSE">
+    <img alt="license" src="https://img.shields.io/github/license/d-velop/dvelop-sdk-node?style=for-the-badge">
+  </a>
   </br>
-  <p>This package is an extensions for <a href="https://github.com/axios/axios">axios</a> to follow links according to <a href="https://stateless.group/hal_specification.html">Hypertext Application Language (hal-json). Hal-Json is widely used in the d.velop cloud.</a></p>
-  <a href="https://d-velop.github.io/dvelop-sdk-node/modules/axios-hal-json.html"><strong>Explore the docs »</strong></a>
+  <p>This package is an extensions for <a href="https://github.com/axios/axios">axios</a> to follow links according to <a href="https://stateless.group/hal_specification.html">Hypertext Application Language (hal-json)</a>. Hal-Json is widely used in the d.velop cloud.</p>
+  <a href="https://d-velop.github.io/dvelop-sdk-node/modules/axios_hal_json.html">
+    <strong>Explore the docs »</strong>
+  </a>
   </br>
-  <a href="https://www.npmjs.com/package/@dvelop-sdk/axios-hal-json"><strong>Install via npm »</strong></a>
+  <a href="https://www.npmjs.com/package/@dvelop-sdk/axios-hal-json">
+    <strong>Install via npm »</strong>
+  </a>
   </br>
-  <a href="https://github.com/d-velop/dvelop-sdk-node"><strong>Check us out on GitHub »</strong></a>
+  <a href="https://github.com/d-velop/dvelop-sdk-node">
+    <strong>Check us out on GitHub »</strong>
+  </a>
 </div>
  * @module axios-hal-json
  */
