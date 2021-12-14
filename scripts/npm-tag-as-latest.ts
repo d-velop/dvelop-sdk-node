1db6da96ab79e7d5ff5a00772b431bf0837d86ea
Lennart <lennart.klose@d-velop.de>
2021-12-14 09:40:24 +0100

    Do not remove next tag

--- scripts/npm-tag-as-latest.ts
+++ scripts/npm-tag-as-latest.ts
@@ -9,7 +9,6 @@ import { execSync } from "child_process";
     const next: string = packages[packageName]['dist-tags'].next;
 
     execSync(`npm dist-tag add ${packageName}@${next} latest`);
-    execSync(`npm dist-tag rm ${packageName} next`);
     console.log(`Tagged ${packageName}@${next} as latest an removed next tag.`);
   });
 })();
