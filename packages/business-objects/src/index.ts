f1becea97623d1f549f2f6fce7f19ae3bc71fd73
Lennart <lennart.klose@d-velop.de>
2022-01-10 12:02:36 +0100

    Created business-objects package

---
+++ packages/business-objects/src/index.ts
@@ -0,0 +1,28 @@
+/**
+<div align="center">
+  <h1>@dvelop-sdk/business-objects</h1>
+  <a href="https://www.npmjs.com/package/@dvelop-sdk/business-objects">
+    <img alt="npm (scoped)" src="https://img.shields.io/npm/v/@dvelop-sdk/business-objects?style=for-the-badge">
+  </a>
+  <a href="https://www.npmjs.com/package/@dvelop-sdk/business-objects">
+    <img alt="npm bundle size (scoped)" src="https://img.shields.io/bundlephobia/min/@dvelop-sdk/business-objects?style=for-the-badge">
+  </a>
+  <a href="https://github.com/d-velop/dvelop-sdk-node">
+    <img alt="GitHub" src="https://img.shields.io/badge/GitHub-dvelop--sdk--node-%23ff0844?logo=github&style=for-the-badge">
+  </a>
+  <a href="https://github.com/d-velop/dvelop-sdk-node/blob/master/LICENSE">
+    <img alt="license" src="https://img.shields.io/github/license/d-velop/dvelop-sdk-node?style=for-the-badge">
+  </a
+  </br>
+  <p>This package contains functionality for the <a href="https://developer.d-velop.de/documentation/business-objectsap/en/dms-api-126976273.html">DMS-App</a> in the d.velop cloud.</p>
+  <a href="https://d-velop.github.io/dvelop-sdk-node/modules/business-objects.html"><strong>Explore the docs »</strong></a>
+  </br>
+  <a href="https://www.npmjs.com/package/@dvelop-sdk/business-objects"><strong>Install via npm »</strong></a>
+  </br>
+  <a href="https://github.com/d-velop/dvelop-sdk-node"><strong>Check us out on GitHub »</strong></a>
+</div>
+ * @module business-objects
+ */
+
+//Utils
+export { DvelopContext, BadInputError, UnauthorizedError, ForbiddenError, NotFoundError } from "@dvelop-sdk/core";
