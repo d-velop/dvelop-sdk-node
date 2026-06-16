3690a7d921a5767e00e55f09da4589649d31b2d5
Lennart Klose <lenklose@gmail.com>
2026-06-16 11:07:27 +0200

    feat: switch to fetch in repository and mapping functions

--- packages/dms/src/repositories/get-repositories/get-repositories.ts
+++ packages/dms/src/repositories/get-repositories/get-repositories.ts
@@ -1,5 +1,5 @@
-import { DvelopContext } from "../../index";
-import { HttpConfig, HttpResponse, _defaultHttpRequestFunction } from "../../utils/http";
+import { DvelopContext, DvelopOptions, dvelopFetch } from "@dvelop-sdk/core";
+import { ensureSuccessResponse } from "../../utils/dms-error";
 import { Repository } from "../get-repository/get-repository";
 
 /**
@@ -7,34 +7,14 @@ import { Repository } from "../get-repository/get-repository";
  * @internal
  * @category Repository
  */
-export function _getRepositoriesDefaultTransformFunction(response: HttpResponse, _: DvelopContext): Repository[] {
-  return response.data.repositories.map((repositoryDto: any) => {
-    return {
-      repositoryId: repositoryDto.id,
-      name: repositoryDto.name,
-      sourceId: repositoryDto._links["source"].href
-    };
-  });
-}
-
-/**
- * Factory for {@link getRepositories}-function. See [Advanced Topics](https://github.com/d-velop/dvelop-sdk-node#advanced-topics) for more information.
- * @typeparam T Return type of the getRepositories-function. A corresponding transformFunction has to be supplied.
- * @internal
- * @category Repository
- */
-export function _getRepositoriesFactory<T>(
-  httpRequestFunction: (context: DvelopContext, config: HttpConfig) => Promise<HttpResponse>,
-  transformFunction: (response: HttpResponse, context: DvelopContext) => T
-): (context: DvelopContext) => Promise<T> {
-  return async (context: DvelopContext) => {
-    const response: HttpResponse = await httpRequestFunction(context, {
-      method: "GET",
-      url: "/dms",
-      follows: ["allrepos"]
-    });
-    return transformFunction(response, context);
-  };
+export async function onResponse(response: Response): Promise<Repository[]> {
+  await ensureSuccessResponse(response);
+  const data: any = await response.json();
+  return data.repositories.map((repo: any) => ({
+    repositoryId: repo.id,
+    name: repo.name,
+    sourceId: `/dms/r/${repo.id}/source`
+  }));
 }
 
 /**
@@ -47,13 +27,17 @@ export function _getRepositoriesFactory<T>(
  *   systemBaseUri: "https://steamwheedle-cartel.d-velop.cloud",
  *   authSessionId: "dQw4w9WgXcQ"
  * });
- * const repoList: string = repos.map(r => r.name).join(", ");
- * console.log("Repositories:", repoList); // Booty Bay, Everlook, Gadgetzan, Ratchet
  * ```
  *
  * @category Repository
  */
-/* istanbul ignore next */
-export async function getRepositories(context: DvelopContext): Promise<Repository[]> {
-  return await _getRepositoriesFactory(_defaultHttpRequestFunction, _getRepositoriesDefaultTransformFunction)(context);
+export async function getRepositories(context: DvelopContext): Promise<Repository[]>;
+export async function getRepositories<T>(context: DvelopContext, options: DvelopOptions<T>): Promise<T>;
+export async function getRepositories<T>(
+  context: DvelopContext,
+  options: DvelopOptions<T | Repository[]> = {
+    onResponse: onResponse
+  }
+): Promise<T | Repository[]> {
+  return dvelopFetch(context, "/dms/r", { method: "GET" }, options);
 }
